f62d9b94aae1059cd0c0212ab9aebc2ea37356c6
Madeline OLeary <madeline.w.oleary@gmail.com>
2021-01-29 09:15:23 +0100

    removing useless method

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -13,10 +13,6 @@ class Topic < ApplicationRecord
     Topic.where("title ILIKE ?", "%#{query}%")
   end
 
-  def self.search_by_topic_service(query)
-    Topic.joins(:point).where("points.name ILIKE ?", "%#{query}%")
-  end
-
   def self.topic_use_frequency
     Topic.where.not(id: 53).includes(:cases).joins(cases: :points).group(:id).order('COUNT(points.id) DESC')
   end
