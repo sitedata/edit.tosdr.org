cf53d7bc52b4dc7be2c4f740f3def0197c70d5a0
Madeline OLeary <madeline.w.oleary@gmail.com>
2021-01-29 09:15:35 +0100

    topic unit tests

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -4,4 +4,18 @@ RSpec.describe Topic, type: :model do
   it "is valid" do
     expect(FactoryBot.build(:topic)).to be_valid
   end
+
+  describe "invalid" do
+    it "lacks a title" do
+      expect(FactoryBot.build(:topic, title: "")).to_not be_valid
+    end
+
+    it "lacks a subtitle" do
+      expect(FactoryBot.build(:topic, subtitle: "")).to_not be_valid
+    end
+
+    it "lacks a description" do
+      expect(FactoryBot.build(:topic, description: "")).to_not be_valid
+    end
+  end
 end
